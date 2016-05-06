b401fd90ea9f69814616aa8077cce1d913f98f72
laurenmholloway <hello@laurenmholloway.com>
2016-05-07 05:30:51 +0900

    added random numbers to mega millions page.

--- www/myScripts/scripts.js
+++ www/myScripts/scripts.js
@@ -8,10 +8,18 @@ function getRando(number){
     return num;
 }
 function getPBNums(){
-    document.getElementById('PB1').innerHTML = getRando(65);
-    document.getElementById('PB2').innerHTML = getRando(65);
-    document.getElementById('PB3').innerHTML = getRando(65);
-    document.getElementById('PB4').innerHTML = getRando(65);
-    document.getElementById('PB5').innerHTML = getRando(65);
-    document.getElementById('PB6').innerHTML = getRando(65);
+    document.getElementById('PB1').innerHTML = getRando(75);
+    document.getElementById('PB2').innerHTML = getRando(75);
+    document.getElementById('PB3').innerHTML = getRando(75);
+    document.getElementById('PB4').innerHTML = getRando(75);
+    document.getElementById('PB5').innerHTML = getRando(75);
+    document.getElementById('PB6').innerHTML = getRando(75);
+}
+function getMegaNums(){
+    document.getElementById('M1').innerHTML = getRando(75);
+    document.getElementById('M2').innerHTML = getRando(75);
+    document.getElementById('M3').innerHTML = getRando(75);
+    document.getElementById('M4').innerHTML = getRando(75);
+    document.getElementById('M5').innerHTML = getRando(75);
+    document.getElementById('M6').innerHTML = getRando(75);
 }

cd42528128729a3c37e1e8b4075ef6e06b65585a
laurenmholloway <hello@laurenmholloway.com>
2016-05-07 04:49:28 +0900

    Powerball random numbers generator complete!

--- www/myScripts/scripts.js
+++ www/myScripts/scripts.js
@@ -1,4 +1,17 @@
 // This is a JavaScript file
-function sayHi(){
-    alert("This works!");
+function getRando(number){
+    var num = Math.floor((Math.random()*number)+1);
+    num = num.toString();
+    if(num.length < 2){
+        num = "0" + num;
+    }
+    return num;
+}
+function getPBNums(){
+    document.getElementById('PB1').innerHTML = getRando(65);
+    document.getElementById('PB2').innerHTML = getRando(65);
+    document.getElementById('PB3').innerHTML = getRando(65);
+    document.getElementById('PB4').innerHTML = getRando(65);
+    document.getElementById('PB5').innerHTML = getRando(65);
+    document.getElementById('PB6').innerHTML = getRando(65);
 }

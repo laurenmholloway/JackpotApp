b0402986f75997f95a9f73559b76776698cf9dfd
laurenmholloway <hello@laurenmholloway.com>
2016-05-07 05:50:46 +0900

    Added random numbers to pick3 and pick4. Changed the rando function to round up or down to help with the randomization.

--- www/myScripts/scripts.js
+++ www/myScripts/scripts.js
@@ -1,6 +1,6 @@
 // This is a JavaScript file
 function getRando(number){
-    var num = Math.floor((Math.random()*number)+1);
+    var num = Math.round((Math.random()*number)+1);
     num = num.toString();
     if(num.length < 2){
         num = "0" + num;
@@ -8,18 +8,29 @@ function getRando(number){
     return num;
 }
 function getPBNums(){
-    document.getElementById('PB1').innerHTML = getRando(75);
-    document.getElementById('PB2').innerHTML = getRando(75);
-    document.getElementById('PB3').innerHTML = getRando(75);
-    document.getElementById('PB4').innerHTML = getRando(75);
-    document.getElementById('PB5').innerHTML = getRando(75);
-    document.getElementById('PB6').innerHTML = getRando(75);
+    document.getElementById('PB1').innerHTML = getRando(74);
+    document.getElementById('PB2').innerHTML = getRando(74);
+    document.getElementById('PB3').innerHTML = getRando(74);
+    document.getElementById('PB4').innerHTML = getRando(74);
+    document.getElementById('PB5').innerHTML = getRando(74);
+    document.getElementById('PB6').innerHTML = getRando(74);
 }
 function getMegaNums(){
-    document.getElementById('M1').innerHTML = getRando(75);
-    document.getElementById('M2').innerHTML = getRando(75);
-    document.getElementById('M3').innerHTML = getRando(75);
-    document.getElementById('M4').innerHTML = getRando(75);
-    document.getElementById('M5').innerHTML = getRando(75);
-    document.getElementById('M6').innerHTML = getRando(75);
+    document.getElementById('M1').innerHTML = getRando(74);
+    document.getElementById('M2').innerHTML = getRando(74);
+    document.getElementById('M3').innerHTML = getRando(74);
+    document.getElementById('M4').innerHTML = getRando(74);
+    document.getElementById('M5').innerHTML = getRando(74);
+    document.getElementById('M6').innerHTML = getRando(74);
+}
+function get3(){
+    document.getElementById('P31').innerHTML = getRando(8);
+    document.getElementById('P32').innerHTML = getRando(8);
+    document.getElementById('P33').innerHTML = getRando(8);
+}
+function get4(){
+    document.getElementById('P41').innerHTML = getRando(8);
+    document.getElementById('P42').innerHTML = getRando(8);
+    document.getElementById('P43').innerHTML = getRando(8);
+    document.getElementById('P44').innerHTML = getRando(8);
 }
